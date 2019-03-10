af0ecd3e1ca0bf425bb835cc412ec687674569c1
Michael Marshall <michael.marshall@ed.ac.uk>
2019-03-10 22:40:14 +0000

    Initial Commit

---
+++ HelloRoot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloRoot
+//
+//  Created by s1786208 on 10/03/2019.
+//  Copyright © 2019 sopa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
